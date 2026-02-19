029de18449ffd872530d9269204ebac19e91558e
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2026-02-19 19:12:35 +0000

    switch to mastua

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ import time
 start_time = time.time() # so we can time runtime
 models_dir = "/home/lucy/Models/" # store our models here
 
-# Mitsua Stable Diffusion requres we specifiy float32
-# pipe = StableDiffusionPipeline.from_pretrained("/home/lucy/Models/mitsua-diffusion-one", torch_dtype=torch.float32, low_cpu_mem_usage=True)
+# Name of model
+model_name = "mitsua-diffusion-one"  # Pick a  model from the Models folder
 
 # Stable diffusion v1.5
-model_name = "stable-diffusion-v1-5" # Pick a  model from the Models folder
+# model_name = "stable-diffusion-v1-5" # Pick a  model from the Models folder
+
 model = models_dir + model_name
-pipe = StableDiffusionPipeline.from_pretrained(model, low_cpu_mem_usage=True)
+pipe = StableDiffusionPipeline.from_pretrained(model, torch_dtype=torch.float32, low_cpu_mem_usage=True)
 pipe = pipe.to("cpu")
 
 prompt = "a photograph of an astronaut riding a horse on mars"
