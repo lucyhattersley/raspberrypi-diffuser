0c9aa0155d86ba4f1bf4d06f87356cd3f0b934e0
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2026-02-19 14:55:48 +0000

    Add code to append runtime to filename

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from diffusers import StableDiffusionPipeline
 from PIL import Image
 import torch
+import time
+ 
+start_time = time.time() # so we can time runtime
+models_dir = "/home/lucy/Models/" # store our models here
 
-# Mitsua Stable Diffusion requres we specifiy float3
+# Mitsua Stable Diffusion requres we specifiy float32
 # pipe = StableDiffusionPipeline.from_pretrained("/home/lucy/Models/mitsua-diffusion-one", torch_dtype=torch.float32, low_cpu_mem_usage=True)
 
 # Stable diffusion v1.5
-pipe = StableDiffusionPipeline.from_pretrained("/home/lucy/Models/stable-diffusion-v1-5", low_cpu_mem_usage=True)
+model_name = "stable-diffusion-v1-5" # Pick a  model from the Models folder
+model = models_dir + model_name
+pipe = StableDiffusionPipeline.from_pretrained(model, low_cpu_mem_usage=True)
 pipe = pipe.to("cpu")
 
-prompt = "a photograph of an astronaut riding a horse"
-image = pipe(prompt, num_inference_steps=31, width=400, height=400).images[0]
+prompt = "a photograph of an astronaut riding a horse on mars"
+image = pipe(prompt, num_inference_steps=35, width=512, height=512).images[0]
 
-image.save("astrohorse-stable.png")
+elapsed_time = time.time() - start
+filename = f"astro-horse-mars_{model}_{elapsed_time:.2f}s.png"
+image.save(filename)
