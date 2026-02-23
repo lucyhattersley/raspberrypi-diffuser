797aee29789dba7c06ed135d55a728460c57039a
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2026-02-23 16:23:51 +0000

    Add app.py back

---
+++ app.py
@@ -0,0 +1,10 @@
+from diffusers import StableDiffusionPipeline
+from PIL import Image
+
+pipe = StableDiffusionPipeline.from_pretrained("../stable-diffusion-v1-5", low_cpu_mem_usage=True)
+pipe = pipe.to("cpu")
+
+prompt = "an astronaut riding a horse on mars"
+image = pipe(prompt, num_inference_steps=5, width=640, height=640).images[0]
+
+image.save("astronaut_horse.png")
