8ea0b79bfbd6579d2a00a92fc6288dded0780533
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2026-02-23 16:51:38 +0000

    Updated comments

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import os
+import os # We need this to expand ~ to your actual home dir
 from diffusers import StableDiffusionPipeline
 from PIL import Image
 
-# We need to expand the user's home directory to get the path to the model
+# The expanded path to the model
 model = os.path.expanduser("~/Models/stable-diffusion-v1-5")
 
 pipe = StableDiffusionPipeline.from_pretrained(model, low_cpu_mem_usage=True) # Load the model
@@ -11,4 +11,4 @@ pipe = pipe.to("cpu") # Move the model to the CPU
 prompt = "an astronaut riding a horse on mars" # Set the prompt
 image = pipe(prompt, num_inference_steps=5, width=640, height=640).images[0]
 
-image.save("astronaut_horse.png")
+image.save("astro-horse.png") 
