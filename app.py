54d7df1b448e8c8ed504065b8baa688c94cfc6a1
Lucy Hattersley <lucy.a.hattersley@gmail.com>
2026-02-23 16:53:35 +0000

    Remove "on mars" from prompt

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = os.path.expanduser("~/Models/stable-diffusion-v1-5")
 pipe = StableDiffusionPipeline.from_pretrained(model, low_cpu_mem_usage=True) # Load the model
 pipe = pipe.to("cpu") # Move the model to the CPU
 
-prompt = "an astronaut riding a horse on mars" # Set the prompt
+prompt = "an astronaut riding a horse" # Set the prompt
 image = pipe(prompt, num_inference_steps=5, width=640, height=640).images[0]
 
 image.save("astro-horse.png") 
